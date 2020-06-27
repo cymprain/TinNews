8fbab58164c16487783397ced28f258ec3d0e9d7
cymprain <yingchen199@gmail.com>
2020-06-27 14:34:49 -0700

    UI Optimization

--- app/src/main/java/com/laioffer/tinnews/database/AppDatabase.java
+++ app/src/main/java/com/laioffer/tinnews/database/AppDatabase.java
@@ -1,4 +1,11 @@
 package com.laioffer.tinnews.database;
 
-public class AppDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.laioffer.tinnews.model.Article;
+
+@Database(entities = {Article.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract RoomDao dao();
 }
--- app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
+++ app/src/main/java/com/laioffer/tinnews/ui/home/HomeViewModel.java
@@ -1,6 +1,40 @@
 package com.laioffer.tinnews.ui.home;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
+import com.laioffer.tinnews.model.Article;
+import com.laioffer.tinnews.model.NewsResponse;
+import com.laioffer.tinnews.repository.NewsRepository;
+
 public class HomeViewModel extends ViewModel {
+    private final NewsRepository repository;
+    private final MutableLiveData<String> countryInput = new MutableLiveData<>(); // input -> pipe -> output
+    private final MutableLiveData<Article> favoriteArticleInput = new MutableLiveData<>();
+
+
+    public HomeViewModel(NewsRepository repository) {
+        this.repository = repository;
+    }
+
+    public void setCountryInput(String country) {
+        countryInput.setValue(country);
+    }
+
+    public void setFavoriteArticleInput(Article article) {
+        favoriteArticleInput.setValue(article);
+    }
+
+    public LiveData<Boolean> onFavorite() {
+        return Transformations.switchMap(favoriteArticleInput, repository:: favoriateArticle);
+    }
+    public LiveData<NewsResponse> getTopHeadlines() {
+        return Transformations.switchMap(countryInput, repository :: getTopHeadlines);
+    }
+
+    public void onCancel() {
+        repository.onCancel();
+    }
 }
